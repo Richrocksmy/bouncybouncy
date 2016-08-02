0ac9d5a9798160053578cd1dcf8c491b4f4322c6
Rich J <rich@23squared.com>
2016-08-02 19:39:01 +0100

    Adding code to create sprite in click location

---
+++ app/src/app.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', () => {
+        var animator = new Animator();
+        
+        document.body.addEventListener('click', () => animator.addSprite(event), true);
+
+        // window.addEventListener('resize', () => animator.resizeCanvas(), false);
+
+        requestAnimationFrame(() => animator.animate());
+    });
+})();
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,43 @@
+// TODO - Extract dirs out to vars
+
 import gulp from 'gulp';
-import less from 'gulp-less';
-import jshint from 'gulp-jshint';
+import eslint from 'gulp-eslint';
+import babel from 'gulp-babel';
+import webserver from 'gulp-webserver';
 
-gulp.task('default', () => console.log('Default task called'));
+gulp.task('lint', () => {
+    gulp.src('./app/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format());
+        // Linter bug - See README
+        // .pipe(eslint.failOnError());
+});
 
-gulp.task('lint-client', () => {
-    gulp.src('./client/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('js', () => {
+    gulp.src('app/src/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('styles', () => {
+    gulp.src(['app/**/*.html', 'app/css/*.css'])
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('serve', ['build'], () => {
+    gulp.src('build')
+        .pipe(webserver(
+            {
+                open: true,
+                livereload: true
+            }
+    ));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('client/**/*.js', ['default']);
-    gulp.watch('test/client/**/*.js', ['default']);
+    gulp.watch('app/**/*.js', ['js']);
+    gulp.watch(['app/**/*.html', 'app/css/*.css'], ['styles']);
 });
 
-gulp.task('build', ['uglify', 'minify']);
+gulp.task('build', ['js', 'styles']);
+gulp.task('default', ['lint', 'build', 'watch', 'serve']);
